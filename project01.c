1a737c1011285359ffc2edfc053053aadcef5b08
Pooja10011 <itz.pooja0562@gmail.com>
2025-11-03 11:20:09 +0530

    Update project01.c

--- project01.c
+++ project01.c
@@ -1,18 +1,37 @@
- struct Player {
- char name[50];
- int runs;
- int wickets;
- int matches;
- };
- void addPlayer() {
- struct Player p;
- printf("Enter Name: ");
- gets(p.name);
- printf("Enter Runs: ");
- scanf("%d", &p.runs;);
- printf("Enter Wickets: ");
- scanf("%d", &p.wickets;);
- printf("Enter Matches: ");
- scanf("%d", &p.matches;);
- saveToFile(p);
- }
+#include <stdio.h>
+
+#include <string.h>
+
+struct Player (
+
+char name [50];
+
+}; int runs, int wickets; int matches:
+
+void addPlayer(struct Player *p) {
+
+printf("Enter Name: ");
+
+scanf("ts", p->name);
+
+printf("Enter Runs: ");
+
+scanf("id", &p->runs);
+
+printf("Enter Wickets: ");
+
+scanf("d", &p->wickets);
+
+printf("Enter Matches: ");
+
+scanf("id", &p->matches);
+
+void displayPlayer(struct Player p) (
+
+printf("\nName: is Runs: id Wickets: id | Matches: id\n", p.name, p.runs, p.wickets, p.matches);
+
+int main() {
+
+}
+
+struct Player pl; addPlayer(pl); displayPlayer(pl); return 0;
